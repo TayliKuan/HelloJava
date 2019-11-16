9368189a270ae7f3245c14cda6a02e5e3be35431
小官 <@DESKTOP-EF5NK7C>
2019-11-16 15:46:19 +0800

    add netneams javaclass

---
+++ Javaclass/src/javaclass/Javaclass.java
@@ -0,0 +1,24 @@
+public class Javaclass{
+	public static void main(String[]text){
+boolean b=true;
+b=false;
+System.out.println(b);//若要印出b=false (左邊b=是固定的 可以用字串 false 是變數)
+System.out.print("b=");
+System.out.print(b);
+char c = 'A';
+System.out.println(c);
+c = '@';
+System.out.println(c);
+c = '我';
+System.out.println(c);
+c = '\'';//使用單引號 印出'
+System.out.println(c);
+c = '\\';//使用單引號 印出\
+System.out.println(c);
+c = '\u0811';//使用單引號 印出你要的萬國碼 Unicode
+System.out.println(c);
+String s ="Hello Java";
+System.out.println(s);
+	}}
+
+//
---
+++ Javaclass/src/javaclass/newClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaclass;
+
+/**
+ *
+ * @author 小官
+ */
+public class newClass {
+
+}
