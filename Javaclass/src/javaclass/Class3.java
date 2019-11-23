5d41b06c945ab66ba1d17365ee8f8902d6aafa3a
小官 <@DESKTOP-C0T6559>
2019-11-23 13:15:11 +0800

    if 簡寫

--- Javaclass/src/javaclass/Class3.java
+++ Javaclass/src/javaclass/Class3.java
@@ -35,10 +35,64 @@ public class Class3 {
     }
 }
 */
+/*九九
 public class Class3 {
     public static void main(String[] args) {
-        for (int  i=1 ; i<11;i++) {
-            System.out.print( i+" ");}
-            System.out.print("\n");
+        //for (int  i=1 ; i<11;i++) {
+           // System.out.print( i+" ");}
+            //System.out.print("\n");
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                if(i*j<10){
+                System.out.print(i + "*" + j +"= "+ i*j+" ");
+                }else{
+                System.out.print(i + "*" + j +"="+ i*j+" ");   
+                    //System.out.printf(" %2d*%-2d=%-3d",i,j,i*j);//2d兩格 -就是靠左還是右
+                }}
+        System.out.println("");
+        }//but三位數不行
+    }
+}*/
+/*99三角
+public class Class3 {
+    public static void main(String[] args) {
+        //for (int  i=1 ; i<11;i++) {
+           // System.out.print( i+" ");}
+            //System.out.print("\n");
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <=i; j++) {
+                if(i*j<10){
+                System.out.print(i + "*" + j +"= "+ i*j+" ");
+                }else{
+                System.out.print(i + "*" + j +"= "+ i*j+" ");   
+                    //System.out.printf(" %2d*%-2d=%-3d",i,j,i*j);//2d兩格 -就是靠左還是右
+                }}
+        System.out.println("");
+        }//but三位數不行
     }
 }
+*/
+//if簡寫
+/*
+public class Class3 {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("請輸入想要列印的次數");
+        int num = sc.nextInt();//num是不變的 所以要在另一個變數 來用迴圈
+        for (int i = 1; i<=num; i++) {
+            System.out.println(i);
+        }
+        System.out.println(num+" is "+((num%2 == 0)?"even":"odd"));//?代表if true執行?後面字串 如果false執行:後面字串
+        //簡寫以下if else 這用法只用在二擇一
+        /*if(num%2==0 )
+            System.out.println(num+"is even");
+        else
+            System.out.println(num+"is odd");
+        }
+    }
+*/
+public class Class3 {
+    public static void main(String[] args) {
+        
+        }
+    }
