fe666deb803ff8529b3e6492068f7db4afd3f8ff
小官 <@DESKTOP-C0T6559>
2019-11-26 21:17:08 +0800

    水仙花數

--- Javaclass/src/Class3hw.java
+++ Javaclass/src/Class3hw.java
@@ -39,7 +39,27 @@
     }
 }
 */
-
+//解2 判斷幾位數
+public class Class3hw {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("請輸入任一數字 判斷此數為幾位數");
+        int enternum = sc.nextInt();//固定
+        int a=1;//變數 /10判斷 再加一位回來
+            if(enternum/10!=0){//輸入值不等於0
+                while(enternum/10!=0){
+                enternum=enternum/10;//以除以10判斷有多少位數
+                a=a+1;}//除以10會少一位 要加一位回來
+                System.out.println("此數為"+a+"位數");
+            }else if(enternum/10==0){//輸入值為個位數/10一定為0
+            System.out.println("此數為個位數");
+        }
+        
+    }
+} 
+        
+        
+/*
 //讓使用者輸入一個三位數資料，如 123，或是776 ，判斷輸入的數字是否為水仙花數?!
 //153 = 1的3次方 + 5的3次方 + 3的3次方
 //370 = 3的3次方 + 7的3次方 + 0的3次方
@@ -54,15 +74,19 @@ public class Class3hw {
             x=inputnum/100;
             y=(inputnum-x*100)/10;
             z=inputnum-x*100-y*10;
-            
-            int a;
-            a=x*x*x+y*y*y+z*z*z;
-            boolean b=true;
-                if (a==inputnum){
-                    b=true;
+            if(inputnum==(x*x*x)+(y*y*y)+(z*z*z))
                 System.out.println("是水仙花數");
-                }else{
-                    b=false;
-                    System.out.println("不為水仙花數");}
+            else
+                System.out.println("不為水仙花數");
+            //int a;
+            //a=x*x*x+y*y*y+z*z*z;
+           // boolean b=true;
+                //if (a==inputnum){
+                   // b=true;
+                //System.out.println("是水仙花數");
+                //}else{
+                    //b=false;
+                    //System.out.println("不為水仙花數");}
     }
 }
+*/
