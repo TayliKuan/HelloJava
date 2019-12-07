02ffc33df0aff454d5e05a9b55a6ec8748e304e5
小官 <@DESKTOP-C0T6559>
2019-12-07 17:45:21 +0800

    中階01 物件導向

--- Javaclass/src/Intermediate/Class5.java
+++ Javaclass/src/Intermediate/Class5.java
@@ -1,15 +1,20 @@
-package Intermediate;
 
+package Intermediate;
+import java.awt.Point;
+import javax.swing.JFrame;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+
 /**
  *
  * @author 小官
  */
+ 
+
 class Car{//類別 類別名稱
     //以下 非功能 (變數) 屬性
     double price;
@@ -26,11 +31,15 @@ class Car{//類別 類別名稱
 
 public class Class5 {
     public static void main(String[] args) {
-        MyPoint p1 = new MyPoint();
-        p1.setX(5);
-        p1.setY(6);
-        System.out.println("p1.getX()="+p1.getX());
-        System.out.println("p1.getY()="+p1.getY());
+        MyPoint p1 = new MyPoint(6,8);//在這裡MyPoint就被呼叫了 建構子 只有new可以
+//        p1.setX(5);
+//        p1.setY(6);
+//        System.out.println("p1.getX()="+p1.getX());
+//        System.out.println("p1.getY()="+p1.getY());
+        p1.print();
+        p1 = new MyPoint(6);
+        p1.print();
+        p1 = new MyPoint();
         p1.print();
 //        Car c1 = new Car();
 //        c1.brand = "toyota";
@@ -48,5 +57,18 @@ public class Class5 {
 //        Car c2 = new Car();
 //        System.out.println("c2.brand"+c2.brand);
 //        System.out.println("c2.speed"+c2.speed);
+    Point test=new Point(6,8);
+        System.out.println(test);
+    test.move(3,4);
+        System.out.println(test);
+        
+    JFrame jf = new JFrame("Hello World");
+    jf.setVisible(true);
+    //一個網頁視窗
+    jf.setSize(300, 300);//像素
+    jf.setAlwaysOnTop(true);//在最上面
+    jf.setEnabled(true);//不能拖 不能移動
+    jf.setOpacity(0.5f);//透明度 不能用
+    jf.setResizable(true);//調整視窗大小
     }
 }
--- Javaclass/src/Intermediate/MyPoint.java
+++ Javaclass/src/Intermediate/MyPoint.java
@@ -2,12 +2,34 @@ package Intermediate;
 
 
 public class MyPoint {
-    int x;
-    int y;
+    private int x;
+    private int y;
     int print;
     
+    //如果加了 void MyPoint 變成一般函式 印出變成(0:0)
+//    MyPoint(int i,int j){//建構子 在NEW的時候就被呼叫 像是預設值 針對成員裡面的物件變數做初始化
+//        //可以() 或是給使用著一開始就輸入他要的數字 (int i,int j)很方便
+//        //參數區看狀況做調整
+//        System.out.println("hello");
+//        x = i;
+//        y = j;
+//        print();//都可以呼叫
+//    }
+    MyPoint(int i){
+        x=i;
+        y=i;
+    }
+    MyPoint(){
+        System.out.println("ya");
+    }
+    MyPoint(int x,int y){
+       
+        this.x = x;//沒有this 是0,0 用區域變數 成員變數字進不去
+        this.y = y;
+        print();//都可以呼叫
+    }
     int getX() {
-        return x;
+        return this.x;
     }
 
     void setX(int i) {
@@ -23,8 +45,9 @@ public class MyPoint {
         
     }
     void print(){
-        System.out.println("(x,y)@("+getX()+","+getY()+")");
-        System.out.println("(x,y)@("+x+","+y+")");
+        //System.out.println("(x,y)@("+getX()+","+getY()+")");//透過founction比較好 多一個控制
+        System.out.println("(x,y)@("+this.x+","+this.y+")");//成員變數XY 沒加this
+         //也可以 他會去找成員變數
     }
     
 }
