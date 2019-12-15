89660e9e6a6aa37b37fa79412c9f1031aef373fd
小官 <@DESKTOP-C0T6559>
2019-12-15 23:55:44 +0800

    1A2B FINAL

--- Javaclass/src/Intermediate/A1B2.java
+++ Javaclass/src/Intermediate/A1B2.java
@@ -5,7 +5,6 @@ public class A1B2{
     public static void main(String[] args) {
         
         //印出四位亂數 且數字不重複
-        //String numStr="";
         String Ans = "";
         for (int i = 0; i < 4; i++) {
             Ans = Ans + String.valueOf((int)(Math.random()*10));
@@ -20,34 +19,38 @@ public class A1B2{
 
         java.util.Scanner sc = new java.util.Scanner(System.in);
         System.out.println("1A2B小遊戲 請輸入4位數字 數字不重複");  
-        String guess = sc.next();
-        
-        
-        if(guess.length()>4){
-                System.out.println("長度不符合 請輸入四位數字");
-        }
-        if(Ans != guess){
-        int A=0,B=0;
-                //A 0~3或正確4
-            for(int i=0;i<4;i++){
-                if(Ans.charAt(i)==guess.charAt(i)){
-                    A++;
-                    break;
+
+        for (int k = 1; k < 2; ) {
+            int A=0,B=0;
+            String guess = sc.next();
+                if(guess.length()>4 && guess.length()<4){
+                        System.out.println("長度不符合 請輸入四位數字");
+                        k--;
                 }
-            }
-                //B 0~4
-            for(int i=0;i<4;i++){
-                for(int j=0;j<4;j++){
-                    if( Ans.charAt(j)==guess.charAt(i)){
-                        B++;
-                        break;
+
+                if(Ans != guess){
+                        //A 0~3
+                    for(int i=0;i<4;i++){//i=0~2 charAt位置
+                        if(Ans.charAt(i)==guess.charAt(i)){
+                            A++;
+                        }
                     }
+                        //B 0~4
+                    for(int i=0;i<4;i++){
+                        for(int j=0;j<4;j++){
+                            if( Ans.charAt(j)==guess.charAt(i)){
+                                B++;
+                            }
+                        }
+                    }
+                    k--;
+                    B=B-A;
+                    System.out.println(A+"A"+B+"B");
+                    
+                    if (A==4) {
+                    System.out.println("你答對了");
                 }
             }
-            System.out.println(A+"A"+B+"B");
-        }   
-        else if(Ans == guess){
-            System.out.println("你答對了!");
-        }        
+        } 
     }
 }
