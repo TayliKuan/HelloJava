02ede78888a2ba0f6359b322352826731fe5ba6d
小官 <@DESKTOP-C0T6559>
2020-01-14 21:20:19 +0800

    JF 1A2B

---
+++ Javaclass/src/A1B2/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A1B2;
+
+import A1B2.Demo1A2B;
+
+/**
+ *
+ * @author 小官
+ */
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("1A2B小遊戲 請輸入4位數字 數字不重複");  
+        String guess = sc.next();
+       
+   
+    }
+}
